7ee61aa84873f48035b0822ce8eb581755d31763
facundonegoci <negocifacundo@hotmail.com>
2017-04-25 17:50:14 -0300

    Adjustments in files config, routes and TestAivo

--- appV1.0.0/config/config.php
+++ appV1.0.0/config/config.php
@@ -184,7 +184,7 @@ $config['directory_trigger'] = 'd'; // experimental not currently in use
   | your log files will fill up very fast.
   |
  */
-$config['log_threshold'] = 4;
+$config['log_threshold'] = 1;
 
 /*
   |--------------------------------------------------------------------------
@@ -195,7 +195,7 @@ $config['log_threshold'] = 4;
   | application/logs/ folder. Use a full server path with trailing slash.
   |
  */
-$config['log_path'] = '';
+$config['log_path'] = FCPATH .'appV1.0.0/logs/';
 
 /*
   |--------------------------------------------------------------------------
